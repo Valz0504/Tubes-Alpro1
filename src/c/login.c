9a0a1b8b995778ec49d239d8840590802a62e8fd
Emilio Justin <emilio.justin54@gmail.com>
2025-05-03 22:43:37 +0700

    feat: nambah fitur login, dan memperbaiki header

---
+++ src/c/login.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "header/user.h"
+#include "header/boolean.h"
+
+void login(UserList *list, User *current_user, boolean *isLogin) {
+    char username[100], password[100];
+
+    printf("Username: ");
+    scanf("%s", username);
+    printf("Password: ");
+    scanf("%s", password);
+
+    User *user = findUser(list, username);   // nampung user yang sudah dicari
+    if (user == NULL) {
+        printf("Tidak ada Manager, Dokter, atau pun Pasien yang bernama %s\n", username);
+        return;
+    }
+
+    if (strcmp(user->password, password) == 0) {
+        if (user->role == 0) {
+            printf("Selamat pagi %s! Ada keluhan apa ?\n", user->username);
+        } else if (user->role == 1) {
+            printf("Selamat pagi %s %s!\n", roleToStr(user->role), user->username);
+        } else if (user->role == 2) {
+            printf("Selamat pagi %s %s!\n", roleToStr(user->role) ,user->username);
+        }
+        *current_user = *user;
+        *isLogin = TRUE;
+    } else {
+        printf("Username atau password salah untuk pengguna yang bernama %s\n", username);
+    }
+}
