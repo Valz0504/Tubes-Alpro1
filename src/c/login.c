e52dd81b82993c1bb019b4757f2649ea4cae51e9
Emilio Justin <emilio.justin54@gmail.com>
2025-05-08 09:54:02 +0700

    fix: validasi fitur

--- src/c/login.c
+++ src/c/login.c
@@ -5,32 +5,36 @@
 #include "boolean.h"
 
 void login(UserList *list, User *current_user, boolean *isLogin) {
-    printf("=== LOGIN ===\n");
+    if (!(*isLogin)) {
+        printf("=== LOGIN ===\n");
+        
+        char username[100], password[100];
     
-    char username[100], password[100];
-
-    printf("Username: ");
-    scanf("%s", username);
-    printf("Password: ");
-    scanf("%s", password);
-
-    User *user = findUser(list, username);   // nampung user yang sudah dicari
-    if (user == NULL) {
-        printf("Tidak ada Manager, Dokter, atau pun Pasien yang bernama %s\n\n", username);
-        return;
-    }
-
-    if (strcmp(user->password, password) == 0) {
-        if (user->role == 0) {
-            printf("Selamat pagi %s! Ada keluhan apa ?\n\n", user->username);
-        } else if (user->role == 1) {
-            printf("Selamat pagi %s %s!\n\n", roleToStr(user->role), user->username);
-        } else if (user->role == 2) {
-            printf("Selamat pagi %s %s!\n\n", roleToStr(user->role) ,user->username);
+        printf("Username: ");
+        scanf("%s", username);
+        printf("Password: ");
+        scanf("%s", password);
+    
+        User *user = findUser(list, username);   // nampung user yang sudah dicari
+        if (user == NULL) {
+            printf("Tidak ada Manager, Dokter, atau pun Pasien yang bernama %s\n\n", username);
+            return;
         }
-        *current_user = *user;
-        *isLogin = TRUE;
+    
+        if (strcmp(user->password, password) == 0) {
+            if (user->role == 0) {
+                printf("Selamat pagi %s! Ada keluhan apa ?\n\n", user->username);
+            } else if (user->role == 1) {
+                printf("Selamat pagi %s %s!\n\n", roleToStr(user->role), user->username);
+            } else if (user->role == 2) {
+                printf("Selamat pagi %s %s!\n\n", roleToStr(user->role) ,user->username);
+            }
+            *current_user = *user;
+            *isLogin = TRUE;
+        } else {
+            printf("Username atau password salah untuk pengguna yang bernama %s\n\n", username);
+        }
     } else {
-        printf("Username atau password salah untuk pengguna yang bernama %s\n\n", username);
+        printf("Anda sudah login!\n\n");
     }
 }
