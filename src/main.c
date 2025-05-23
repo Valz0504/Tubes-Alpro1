d76a04217276dfc68e6b5ef206b5a63753fe06fe
Emilio Justin <emilio.justin54@gmail.com>
2025-05-23 10:51:40 +0700

    fix: perbaiki lihat ruangan

--- src/main.c
+++ src/main.c
@@ -40,7 +40,14 @@ int main(int argc, char* argv[]) {
     boolean run_program = TRUE;
     while (run_program) {
         char prompt[100];
+
+        if (isLogin) {
+            printf(GRAY "%s@%s " RESET, current_user.username, roleToStr(current_user.role));
+        } else {
+            printf(GRAY "anon@none " RESET);
+        }
         printf(MAGENTA ">>> Command: " RESET);
+
         scanf("%s", prompt);
         printf("\n");
 
